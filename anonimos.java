9c80303890451bc6d4eea8cdcfb9eceb5ea617e6
suvasanketrout <rout.suvasanket2@icloud.comm>
2023-02-17 11:23:12 +0530

    java newbie stuffs

---
+++ anonimos.java
@@ -0,0 +1,11 @@
+class a {
+  String x = "outside";
+
+  class B {
+    String y = "inside";
+  }
+}
+/**
+ * anonimos
+ */
+public class anonimos {}
---
+++ tets.java
@@ -0,0 +1,13 @@
+public class tets {
+
+  public static void main(String[] args) {
+    System.out.println("hello");
+    linlis ll = new linlis();
+    ll.addFirst(10);
+    ll.addFirst(11);
+    ll.addFirst(17);
+    ll.addFirst(14);
+    ll.addFirst(21);
+    ll.printlinlis();
+  }
+}
